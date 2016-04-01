b61e9c0292ff2945560ce61331632adbc187f86e
aezeylan <aez@MacBook-Pro-van-Emir.local>
2016-04-01 23:11:44 +0200

    file
    file text

---
+++ www/js/file.js
@@ -0,0 +1,23 @@
+function onInitFs(fs) {
+
+  fs.root.getFile('sample.txt', {}, function(fileEntry) {
+
+    // Get a File object representing the file,
+    // then use FileReader to read its contents.
+    fileEntry.file(function(file) {
+       var reader = new FileReader();
+
+       reader.onloadend = function(e) {
+         var txtArea = document.createElement('textarea');
+         txtArea.value = this.result;
+         document.body.appendChild(txtArea);
+       };
+
+       reader.readAsText(file);
+    }, errorHandler);
+
+  }, errorHandler);
+
+}
+
+window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
